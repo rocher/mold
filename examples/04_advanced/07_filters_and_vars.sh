2f22787cccbb5d532666289bc961b48ebf48d0c2
Francesc Rocher <francesc.rocher@gmail.com>
2025-05-25 13:43:11 +0200

    Complex example of filter in var subst

--- examples/04_advanced/07_filters_and_vars.sh
+++ examples/04_advanced/07_filters_and_vars.sh
@@ -2,5 +2,5 @@
 CWD="$(realpath $(dirname $0))"
 source $CWD/../common.sh
 title "Filters and variable substitution"
-cmd mold show-vars filters+vars.toml
+cmd mold show-vars filters+vars.toml | sort
 test cat filters+vars.toml
